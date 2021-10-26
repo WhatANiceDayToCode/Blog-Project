3659bde199ebca4cfddd0996c2186c5658f83b02
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-10-26 21:37:03 +0200

    Update accueil.php

--- pages/accueil.php
+++ pages/accueil.php
@@ -5,7 +5,19 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Accueil</title>
-
+        <script language="javascript" type="text/javascript">
+            function valideSuppr() 
+            {
+                if (confirm("Etes vous sur de vouoir vous déconnecter ?")) 
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        </script>
         <?php
             session_start();
             include_once('../connexion/connexion.php');
@@ -33,7 +45,7 @@
                 echo $message.'<br>';
                 echo ('Bienvenue '.$_SESSION['pseudo'].' / '.$_SESSION['nom'].' '.$_SESSION['prenom']);
                 echo ('<br><br>');
-                echo ('<a href="deconnexion.php"><input type="button" value="Se deconnecter"></a>');
+                echo ('<a href="deconnexion.php"><input type="button" value="Se deconnecter" onclick="return valideSuppr()"></a>');
             }
         ?>
     </body>
