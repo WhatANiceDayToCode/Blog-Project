5dcc209a7f089e2f6e66d8d3316c93baba300cb4
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-10-26 12:09:07 +0200

    sds

---
+++ pages/accueil.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Accueil</title>
+
+        <?php
+            session_start();
+            include_once("../connexion/connexion.php");
+        ?>
+    </head>
+    <body>
+        
+    </body>
+</html>
--- pages/creactionCompte.php
+++ pages/creactionCompte.php
@@ -4,11 +4,11 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Creation de Compte</title>
 
         <?php
             session_start();
-            include_once("../../connexion/connexion.php");
+            include_once("../connexion/connexion.php");
             $valide = true;
 
             if (array_key_exists('nom',$_POST) && array_key_exists('prenom',$_POST) && array_key_exists('mail',$_POST)
