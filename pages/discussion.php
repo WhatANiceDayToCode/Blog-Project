c9379ec091dcdb07bc383090da2f1bf36aae3089
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-10-27 22:12:31 +0200

    Create discussion.php

---
+++ pages/discussion.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- php ici pour modifier le titre de la page  -->
+        <title>Sujet</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
