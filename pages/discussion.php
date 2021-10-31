7b1160ad7b2de45883e77ca46f469790da3865fa
Blue <leanefelis@gmail.com>
2021-10-31 17:34:10 +0100

    affichage des rep + sect com quand co ok

--- pages/discussion.php
+++ pages/discussion.php
@@ -37,30 +37,41 @@
 
         $select_stmt->execute();
 
-
         $sujet = $select_stmt->fetch();
 
         if ($sujet != null) {
             $dateSujet = date('d/m/Y', strtotime($sujet['dateSujet']));
 
             echo ('Titre : ' . $sujet['titreSujet'] . '<br>Par le rédacteur : ' . $sujet['pseudo'] . ' le ' . $dateSujet . '<br><br><br>');
-
             //Ouverture de la table avec le texte du sujet et les reponses correspondantes
             echo ('<table>');
 
             echo ('<tr><td>' . $sujet['texteSujet'] . '</td><td></td></tr>');
             //Inclure toute les reponses avec un select et un foreach
 
+            $idSujet = $_GET['idSujet'];
+            //echo $idSujet; <-- juste un test de vérif
+            
+            $result = $objPdo->query('  SELECT * 
+                                        FROM reponse
+                                        WHERE idSujet = ' . $idSujet);
+            foreach ($result as $row) {
+                echo ('<tr>');
+                //echo ('<td>' . $row["pseudo"] . '</td>');
+                echo ('<td>' . $row["texteReponse"] . '</td>');
+                echo ('</tr>');
+            }
+
             echo ('<table>');
 
+            if ($connecte == true) {
+                // Section commentaires
+                echo ('<br>');
+                echo ('<h1>Commentaires</h1>');
 
-            // Section commentaires
-            echo ('<br>');
-            echo ('<h1>Commentaires</h1>');
-
-            echo ('<form method="POST">');
-            //Affichage du pseudo, et d'un formulaire de commentaire
-            echo ('<h2>Votre pseudo : ' . $pseudo . '</h2>
+                echo ('<form method="POST">');
+                //Affichage du pseudo, et d'un formulaire de commentaire
+                echo ('<h2>Votre pseudo : ' . $pseudo . '</h2>
                     <textarea name="commentaire" placeholder="Votre commentaire..."></textarea>
                     
                     <br />
@@ -70,21 +81,13 @@
                         <input type="button" value="Retour" name="Retour">
                     </a>');
 
-            echo ('</form>');
-
-            if (isset($c_msg)) {
-                echo $c_msg;
+                echo ('</form>');
+            } else {
+                echo('<h2> Veuillez vous connecter pour pouvoir ajouter un commentaire </h2>');
             }
+
+
             echo ('<br /><br />');
-
-            // while($c = $commentaires->fetch()) { $c['pseudo']:</b> <?= $c['commentaire']
-            // else
-            // {
-            //     echo("Aucun sujet n'existe avec ce code, merci de retourner à l'accueil <br><br>");
-            //     echo('<a href="accueil.php"><input type="button" value="Retour a l\'accueil"></a>');
-            // }
-
-            // Fin section commentaires
         } else {
             echo ("Il y a une erreur dans le chargement de la page, merci de revenir à l'accueil<br><br>");
             echo ('<a href="accueil.php"><input type="button" value="Retour a l\'accueil"></a>');
