295df0ae4ef1f42027a0105bb90ff012b4c9701a
Blue <leanefelis@gmail.com>
2021-11-03 09:39:29 +0100

    scroll fixed

--- pages/creerSujet.php
+++ pages/creerSujet.php
@@ -1,76 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Créer un Sujet</title>
 
-        <?php
-            session_start();
-            include_once("../connexion/connexion.php");
-            $message = "";
-            $connecte = $_SESSION['connection'];
-            $valide = true;
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Créer un Sujet</title>
+    <!-- Feuilles de style -->
+    <link rel="stylesheet" href="./style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Zen+Kurenaido&display=swap" rel="stylesheet">
 
-            if ($connecte && array_key_exists('titre', $_POST) && array_key_exists('texteSujet', $_POST)) 
-            {
-                if ($_POST['titre'] != "" && $_POST['texteSujet'] != "") 
-                {
-                    $message = '';
+    <?php
+    session_start();
+    include_once("../connexion/connexion.php");
+    $message = "";
+    $connecte = $_SESSION['connection'];
+    $valide = true;
 
-                    // Recuperer l'ID du redacteur
-                    $select_stmt = $objPdo->prepare('SELECT idRedacteur FROM redacteur WHERE pseudo = ?');
-                    $select_stmt->bindValue(1, trim($_SESSION['pseudo']), PDO::PARAM_STR);
-                    $select_stmt->execute();
-                    $idRedacteur = $select_stmt->fetch()['idRedacteur'];
+    if ($connecte && array_key_exists('titre', $_POST) && array_key_exists('texteSujet', $_POST)) {
+        if ($_POST['titre'] != "" && $_POST['texteSujet'] != "") {
+            $message = '';
 
-                    // Inserer la reponse dans la BD
-                    $insert_stmt = $objPdo->prepare("INSERT INTO sujet (idRedacteur,titreSujet,texteSujet,dateSujet) VALUES(? ,? ,? ,CURRENT_TIMESTAMP())");
-                    $insert_stmt->bindValue(1, $idRedacteur, PDO::PARAM_INT);
-                    $insert_stmt->bindValue(2, trim($_POST['titre']), PDO::PARAM_STR);
-                    $insert_stmt->bindValue(3, trim($_POST['texteSujet']), PDO::PARAM_STR);
-                    $insert_stmt->execute();
+            // Recuperer l'ID du redacteur
+            $select_stmt = $objPdo->prepare('SELECT idRedacteur FROM redacteur WHERE pseudo = ?');
+            $select_stmt->bindValue(1, trim($_SESSION['pseudo']), PDO::PARAM_STR);
+            $select_stmt->execute();
+            $idRedacteur = $select_stmt->fetch()['idRedacteur'];
 
-                    header('Location:'.$_SESSION['provenance']);
-                }
-                else
-                {
-                    $valide = false;
-                    $message = "Merci de bien saisir tout les champs afin de pouvoir créer le sujet";
-                }
-            }
-            
-        ?>
-    </head>
-    <body>
-        <?php
-            if ($message != "") 
-            {
-                echo $message;
-                echo ("<br><br>");
-            }
-        ?>
+            // Inserer la reponse dans la BD
+            $insert_stmt = $objPdo->prepare("INSERT INTO sujet (idRedacteur,titreSujet,texteSujet,dateSujet) VALUES(? ,? ,? ,CURRENT_TIMESTAMP())");
+            $insert_stmt->bindValue(1, $idRedacteur, PDO::PARAM_INT);
+            $insert_stmt->bindValue(2, trim($_POST['titre']), PDO::PARAM_STR);
+            $insert_stmt->bindValue(3, trim($_POST['texteSujet']), PDO::PARAM_STR);
+            $insert_stmt->execute();
+
+            header('Location:' . $_SESSION['provenance']);
+        } else {
+            $valide = false;
+            $message = "Merci de bien saisir tout les champs afin de pouvoir créer le sujet";
+        }
+    }
+
+    ?>
+</head>
+
+<body>
+    <?php
+    if ($message != "") {
+        echo $message;
+        echo ("<br><br>");
+    }
+    ?>
+    <div class="sect_inc">
         <form action="" method="post">
             <?php
-                echo('Merci de saisir les informations suivantes : <br><br>');
-                echo('Titre : ');
-                echo('<br><input type="text" name="titre"');
-                if (!$valide) 
-                {
-                    echo(' value = "'.$_POST['titre'].'"');
-                }
-                echo('><br>');
-                echo('Texte du Sujet :');
-                echo('<br><input type="text" name="texteSujet"');
-                if (!$valide) 
-                {
-                    echo(' value = "'.$_POST['texteSujet'].'"');
-                }
-                echo('><br><br>');
+            echo ('<div class="title">Création d\'un sujet</div> ');
+            echo('<div class="subTitle"> Si possible veuillez vérifier que le sujet n\'a pas déjà été abordé avant de créer le sujet</div><br><hr><br>');
+            // echo('Titre : ');
+            echo ('<input class="input" type="text" placeholder="Titre" name="titre"');
+            if (!$valide) {
+                echo (' value = "' . $_POST['titre'] . '"');
+            }
+            echo ('><br>');
+            // echo('Texte du Sujet :');
+            echo ('<br><textarea class="area input" type="text" placeholder="Texte du Sujet" name="texteSujet"');
+            if (!$valide) {
+                echo (' value = "' . $_POST['texteSujet'] . '"');
+            }
+            echo ('rows="10" cols="100"></textarea><br><br>');
             ?>
-            <input type="submit" value="Créer"><br><br>
+            <input class="button" type="submit" value="Valider"><br><br>
         </form>
-        <a href="accueil.php">Retour à l'accueil</a>  
-    </body>
+        <a href="accueil.php">Retour à l'accueil</a>
+    </div>
+</body>
+
 </html>
