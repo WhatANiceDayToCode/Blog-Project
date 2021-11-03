a741e64ef07eaf03025348f4971211700fdf4d42
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-11-04 00:00:26 +0100

    Update discussion.php

--- pages/discussion.php
+++ pages/discussion.php
@@ -50,107 +50,109 @@
 <body>
     <div class="sect_inc">
         <?php
-        if (array_key_exists('idSujet', $_GET)) {
-            // On recupere uniquement les attributs necessaires ainsi que le pseudo du redacteur
-            $select_stmt = $objPdo->prepare('SELECT idSujet, titreSujet, texteSujet, pseudo, dateSujet 
-                                            FROM sujet s, redacteur r
-                                            WHERE idSujet = ? 
-                                            AND s.idRedacteur = r.idRedacteur');
-            $select_stmt->bindValue(1, trim($_GET['idSujet']), PDO::PARAM_INT);
-
-            $select_stmt->execute();
-
-            $sujet = $select_stmt->fetch();
-
-            // Affichage des sujets
-            if ($sujet != null) {
-                $dateSujet = date('d/m/Y à H:i:s', strtotime($sujet['dateSujet']));
-
-                echo ('<div class="title">' . $sujet['titreSujet'].'</div>');
-                echo ('<div class="info_sujet">');
-                    echo ('<div class="subTitle"> Par le rédacteur : '.$sujet['pseudo'].'</div>');
-                    echo ('<div>Le '.$dateSujet.'</div>');
-                echo ('</div>');
-                echo ('<div class="subject">' . $sujet['texteSujet'].'</div>');
-                echo ('<hr>');
-
-                //Inclure toute les reponses avec un select et un foreach
-                $select_stmt = $objPdo->prepare('SELECT texteReponse, dateRep, pseudo 
-                                                  FROM reponse rep, redacteur redac
-                                                  WHERE idSujet = ?
-                                                  AND rep.idRedacteur = redac.idRedacteur
-                                                  ORDER BY dateRep ASC');
-
-                $select_stmt->bindValue(1, trim($sujet['idSujet']), PDO::PARAM_INT);
+            if (array_key_exists('idSujet', $_GET)) 
+            {
+                // On recupere uniquement les attributs necessaires ainsi que le pseudo du redacteur
+                $select_stmt = $objPdo->prepare('SELECT idSujet, titreSujet, texteSujet, pseudo, dateSujet 
+                                                FROM sujet s, redacteur r
+                                                WHERE idSujet = ? 
+                                                AND s.idRedacteur = r.idRedacteur');
+                $select_stmt->bindValue(1, trim($_GET['idSujet']), PDO::PARAM_INT);
 
                 $select_stmt->execute();
 
-                $result = $select_stmt->fetchAll();
+                $sujet = $select_stmt->fetch();
 
-                echo ('<div id="title_commentaire" class="title">Commentaires : </div><hr>');
-                if ($result != null) 
+                // Affichage des sujets
+                if ($sujet != null) 
                 {
-                    foreach ($result as $reponse) 
+                    $dateSujet = date('d/m/Y à H:i:s', strtotime($sujet['dateSujet']));
+
+                    echo ('<div class="title">' . $sujet['titreSujet'].'</div>');
+                    echo ('<div class="info_sujet">');
+                        echo ('<div class="subTitle"> Par le rédacteur : '.$sujet['pseudo'].'</div>');
+                        echo ('<div>Le '.$dateSujet.'</div>');
+                    echo ('</div>');
+                    echo ('<div class="subject">' . $sujet['texteSujet'].'</div>');
+                    echo ('<hr>');
+
+                    //Inclure toute les reponses avec un select et un foreach
+                    $select_stmt = $objPdo->prepare('SELECT texteReponse, dateRep, pseudo 
+                                                    FROM reponse rep, redacteur redac
+                                                    WHERE idSujet = ?
+                                                    AND rep.idRedacteur = redac.idRedacteur
+                                                    ORDER BY dateRep ASC');
+
+                    $select_stmt->bindValue(1, trim($sujet['idSujet']), PDO::PARAM_INT);
+
+                    $select_stmt->execute();
+
+                    $result = $select_stmt->fetchAll();
+
+                    echo ('<div id="title_commentaire" class="title">Commentaires : </div><hr>');
+                    if ($result != null) 
                     {
-                        $dateSujet = date('d/m/Y à H:i:s', strtotime($reponse['dateRep']));
-
-                        echo('<div class="reponse');
-                        if ($reponse['pseudo'] == $sujet['pseudo']) 
+                        foreach ($result as $reponse) 
                         {
-                            echo (' writer');
+                            $dateSujet = date('d/m/Y à H:i:s', strtotime($reponse['dateRep']));
+
+                            echo('<div class="reponse');
+                            if ($reponse['pseudo'] == $sujet['pseudo']) 
+                            {
+                                echo (' writer');
+                            }
+                            echo ('">');
+                                echo ('<div class="commentaire_pseudo">'.$reponse["pseudo"].'</div>');
+                                echo ($reponse["texteReponse"]);
+                                echo ('<div class="commentaire_date"> le '.$dateSujet.'</div>');
+                            echo ('</div>');
+                            echo ('<hr>');
                         }
-                        echo ('">');
-                            echo ('<div class="commentaire_pseudo">'.$reponse["pseudo"].'</div>');
-                            echo ($reponse["texteReponse"]);
-                            echo ('<div class="commentaire_date"> le '.$dateSujet.'</div>');
-                        echo ('</div>');
-                        echo ('<hr>');
                     }
-                }
 
-                echo ('<table>');
+                    echo ('<table>');
 
 
-                // Afficher la saisie de commentaire uniquement si l'on est connecté
-                echo ('<div class ="section_commentaire">');
-                if ($connecte) 
-                {
-                    // Section reponse
-                    echo ('<div class="title">Ajouter un commentaire</div>');
+                    // Afficher la saisie de commentaire uniquement si l'on est connecté
+                    echo ('<div class ="section_commentaire">');
+                    if ($connecte) 
+                    {
+                        // Section reponse
+                        echo ('<div class="title">Ajouter un commentaire</div>');
 
-                    echo ('<form method="POST">');
+                        echo ('<form method="POST">');
 
-                        //Affichage du pseudo, et d'un formulaire de commentaire
-                        echo ('<div class="subTitle"> Votre pseudo : '.$pseudo.'</div>');
+                            //Affichage du pseudo, et d'un formulaire de commentaire
+                            echo ('<div class="subTitle"> Votre pseudo : '.$pseudo.'</div>');
 
-                        echo ('<textarea class="input area" name="reponse" value="reponse" placeholder="Votre réponse..." rows="5" cols="400"></textarea>');
-                        echo ('<input class="button" type="submit" value="Poster ma réponse" name="submit_reponse"/>');
+                            echo ('<textarea class="input area" name="reponse" value="reponse" placeholder="Votre réponse..." rows="5" cols="400"></textarea>');
+                            echo ('<input class="button" type="submit" value="Poster ma réponse" name="submit_reponse"/>');
 
-                    echo ('</form>');
+                        echo ('</form>');
+                    } 
+                    else 
+                    {
+                        echo ('Veuillez vous connecter pour pouvoir ajouter un commentaire');
+
+                        echo ('<div>');
+                            // Proposition de connexion
+                            echo ('<a href="login.php"><input type="button" class="button" value="Se connecter"></a>');
+                            // Ou d'inscription !
+                            echo (' ou ');
+                            echo ('<a href="creerCompte.php"><input class="button" type="button" value="Créer un compte"></a>');
+                        echo ('</div>');
+                    }
+                    echo ('</div>');
                 } 
                 else 
                 {
-                    echo ('Veuillez vous connecter pour pouvoir ajouter un commentaire');
-
-                    echo ('<div>');
-                        // Proposition de connexion
-                        echo ('<a href="login.php"><input type="button" class="button" value="Se connecter"></a>');
-                        // Ou d'inscription !
-                        echo (' ou ');
-                        echo ('<a href="creerCompte.php"><input class="button" type="button" value="Créer un compte"></a>');
-                    echo ('</div>');
+                    echo ("Aucun sujet n'existe avec ce code, merci de revenir à l'accueil<br><br>");
                 }
-                echo ('</div>');
             } 
             else 
             {
-                echo ("Aucun sujet n'existe avec ce code, merci de revenir à l'accueil<br><br>");
+                echo ("Il y a une erreur dans le chargement de la page, merci de revenir à l'accueil<br><br>");
             }
-        } 
-        else 
-        {
-            echo ("Il y a une erreur dans le chargement de la page, merci de revenir à l'accueil<br><br>");
-        }
         ?>
         <a href="accueil.php">Retour a l'accueil</a>
     </div>
