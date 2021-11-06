5719a96f0083fd9935b28dc3e2a63ab93530589a
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-11-07 00:14:19 +0100

    Update connexion.php

--- connexion/connexion.php
+++ connexion/connexion.php
@@ -6,7 +6,6 @@
         $password = "Xbb8R2D2";
 
         $objPdo = new PDO('mysql:host=devbdd.iutmetz.univ-lorraine.fr;port=3306;dbname='.$dbname, $username, $password);
-        //$objPdo = new PDO('mysql:host=devbdd.iutmetz.univ-lorraine.fr;port=3306;dbname=XXX', 'XXX', 'XXX');
     }
     catch( Exception $exception )
     {
