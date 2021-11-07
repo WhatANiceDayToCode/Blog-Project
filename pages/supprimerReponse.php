bd701acce56b60d4b44254e311918b116d15e4e6
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-11-07 21:01:15 +0100

    Update supprimerReponse.php

--- pages/supprimerReponse.php
+++ pages/supprimerReponse.php
@@ -3,8 +3,13 @@
     session_start();
     $idReponse = $_GET['idReponse'];
     
-    $delete_stmt = $objPdo->prepare
+    $delete_stmt = $objPdo->prepare('DELETE 
+                                     FROM reponse
+                                     WHERE idReponse = ?');
+    
+    $delete_stmt->bindValue(1, trim($idSujet), PDO::PARAM_INT);
 
+    $delete_stmt->execute();
 
     header('Location:'.$_SESSION['provenance']);
 ?>
