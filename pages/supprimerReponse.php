e2a5cadccf0aae94a33c09dd716175c38418cf1a
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-11-07 20:52:51 +0100

    Update supprimerReponse.php

--- pages/supprimerReponse.php
+++ pages/supprimerReponse.php
@@ -1,6 +1,10 @@
 <?php
     include_once('connexion.php');
     session_start();
+    $idReponse = $_GET['idReponse'];
+    
+    $delete_stmt = $objPdo->prepare
+
 
     header('Location:'.$_SESSION['provenance']);
 ?>
