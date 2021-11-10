cc1c8dc637c3e33a8850d2b193093eca9b4042f1
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-11-10 20:42:05 +0100

    a

--- pages/suppressionSujet.php
+++ pages/suppressionSujet.php
@@ -4,24 +4,44 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Sup^pression d'un sujet</title>
+
+        <link rel="stylesheet" href="./style.css">
+        <link href="https://fonts.googleapis.com/css2?family=Zen+Kurenaido&display=swap" rel="stylesheet">
+        <title>Suppression d'un sujet</title>
 
         <?php
             include_once("../connexion/connexion.php");
             session_start();
 
             $idSujet = null;
+            $connecte = false;
+
+            if (array_key_exists('connection', $_SESSION) && $_SESSION['connection'])
+            {
+                $connecte = true;
+            }
 
             if ($connecte && array_key_exists('idSujet', $_GET)) 
             {
                 $idSujet = $_GET['idSujet'];
             }
+
+
         ?>
     </head>
     <body>
         <div class="sect_inc">
             <div class="title">Suppression du sujet</div>
             
+
+
+
+            <div class="bloc_validation">
+                <form action="" method="post">
+                    <input type="button" value="Supprimer">
+                </form>
+                <a href="accueil.php"></a>
+            </div>
         </div>
     </body>
 </html>
